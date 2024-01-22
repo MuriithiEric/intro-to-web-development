93ced93a47f5624f2847af8056cb782bc953365e
MuriithiEric <56841703+MuriithiEric@users.noreply.github.com>
2024-01-22 17:34:44 +0300

    building a 2d game in javascript

---
+++ practice-project-creating-a-2d-game/breakout.js
@@ -0,0 +1,151 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+var gameOverText = document.getElementById("gameOver");
+var startButton = document.getElementById("startButton");
+var interval;
+
+// Define the game logic inside a function
+function startGame() {
+  // Reset everything
+  clearInterval(interval);
+  gameOverText.style.display = "none";
+
+  // Ball
+  var ballRadius = 10;
+  var ballX = canvas.width / 2;
+  var ballY = canvas.height - 30;
+  var dx = 2;
+  var dy = -2;
+
+  // Paddle
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width - paddleWidth) / 2;
+  var rightPressed = false;
+  var leftPressed = false;
+
+  // Bricks
+  var brickRowCount = 5;
+  var brickColumnCount = 3;
+  var brickWidth = 75;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 30;
+  var brickOffsetLeft = 30;
+  var bricks = [];
+  for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+      bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+  }
+
+  // Control
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
+  function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+
+  function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+      for (var r = 0; r < brickRowCount; r++) {
+        var b = bricks[c][r];
+        if (b.status == 1) {
+          if (
+            ballX > b.x &&
+            ballX < b.x + brickWidth &&
+            ballY > b.y &&
+            ballY < b.y + brickHeight
+          ) {
+            dy = -dy;
+            b.status = 0;
+          }
+        }
+      }
+    }
+  }
+
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+      for (var r = 0; r < brickRowCount; r++) {
+        if (bricks[c][r].status == 1) {
+          var brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
+          var brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    collisionDetection();
+
+    if (ballX + dx > canvas.width - ballRadius || ballX + dx < ballRadius) {
+      dx = -dx;
+    }
+    if (ballY + dy < ballRadius) {
+      dy = -dy;
+    } else if (ballY + dy > canvas.height - ballRadius) {
+      if (ballX > paddleX && ballX < paddleX + paddleWidth) {
+        dy = -dy;
+      } else {
+        gameOverText.style.display = "block";
+        clearInterval(interval);
+      }
+    }
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+      paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+
+    ballX += dx;
+    ballY += dy;
+  }
+
+  interval = setInterval(draw, 10);
+}
+
+// Add an event listener to start the game when the start button is clicked
+startButton.addEventListener("click", startGame);
