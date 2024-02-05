c576dbd7140dfb50bbe9b04363d43fa6837a536b
MuriithiEric <56841703+MuriithiEric@users.noreply.github.com>
2024-02-05 20:16:07 +0300

    pushed code on functions

---
+++ Event-Listeners/script.js
@@ -0,0 +1,10 @@
+function displaySum() {
+  //function to display the sum of two numbers
+  let a = 20; //set a variable and givenit a value
+  let b = 30;
+  let sum = a + b; //set a sum variable and give it a value of a + b
+
+  console.log(sum); //print out the value of sum
+}
+
+displaySum();
